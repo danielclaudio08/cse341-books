2c6c900f7ab2790d9041c80a9034e64c921f8706
danielclaudio08 <u262229862@byui.edu>
2026-09-18 01:19:09 +0800

    Week 2 Activity: Swagger Documentation

--- src/router.js
+++ src/router.js
@@ -1,9 +1,89 @@
 import express from 'express';
-import { getBooksHandler, getBookByIdHandler  } from './controllers/books.js';
+import { getBooksHandler, getBookByIdHandler } from './controllers/books.js';
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: A list of books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.get('/books', getBooksHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     summary: Get a book by ID
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the book to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The requested book
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       404:
+ *         description: Book not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 router.get('/books/:id', getBookByIdHandler);
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       description: A book document
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Unique identifier for the book
+ *       additionalProperties: true
+ *     Error:
+ *       type: object
+ *       required:
+ *         - message
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: Internal server error
+ */
 
 export default router;
